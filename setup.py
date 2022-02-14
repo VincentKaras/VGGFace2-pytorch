a376ef139d07dd4022587113444f86a9241a4edd
Vincent <vincentkaras@onlinehome.de>
2022-02-14 18:16:18 +0100

    "added setup.py so it can be installed as a package"

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='VGGFace2-pytorch',
+    version='1.0',
+    packages=['models', 'datasets'],
+    url='https://github.com/VincentKaras/VGGFace2-pytorch',
+    license='MIT',
+    author='VincentKaras',
+    author_email='vincent.karas@tum.de',
+    description='ResNet50 and SENet50 pre-trained on VGGFace2. Forked from https://github.com/cydonia999/VGGFace2-pytorch',
+    install_requires=[
+        "pandas>=1.4",
+        "torch>=1.10",
+        "numpy>=1.19.2",
+    ]
+)
